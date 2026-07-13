369468c6b094f606f02fea156dbb03c647325ded
priyachristina2005-bin <priyachristina2005@gmail.com>
2026-07-13 19:44:04 +0600

    add 2 number

---
+++ sum 2 number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+ int num1, num2,sum;
+ cout<<"enter 2 number";
+ cin>>num1 >>num2;
+ sum=num1+num2;
+ cout<<"Sum is="<<sum;
+ getch();
+}
