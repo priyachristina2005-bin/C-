f8fd9c1aabb4110d55071a255d7b5d3cd302bca2
priyachristina2005-bin <priyachristina2005@gmail.com>
2026-07-13 19:17:26 +0600

    String-added

---
+++ data type, variable , keyword.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int num1=10,num2=20;
+    char ch;
+    ch='a';
+    float num=10.5;
+    double n=1.23455;
+
+getch();
+}
---
+++ string.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+ char name[24]="Priya Christina Rozario";
+ cout << "MY name is ="<<name;
+ getch();
+}
